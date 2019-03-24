0cda9baeeb3c3ff2f5ce4768fe3a0e8e1c010773
Sebastian Bernth Jepsen <sbj@proxus.dk>
2019-03-24 18:45:46 +0100

    .

--- src/main/java/presentation/Command.java
+++ src/main/java/presentation/Command.java
@@ -7,26 +7,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public abstract class Command {
+public abstract class Command
+{
+   
+    public abstract void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 
-    private static HashMap<String, Command> commands;
-
-    public abstract void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, LoginSampleException;
-
+    
     public static Command from(HttpServletRequest request)
     {
         Command c;
         
         String origin = request.getParameter("command");
+       
         
         HashMap<String, Command> commands;
+
 
         commands = new HashMap<>();
         commands.put("login", new CommandLogin());
         commands.put("register", new CommandRegister());
         commands.put("mainPage", new CommandMainPage());
 
-        c = commands.getOrDefault(origin, new CommandUnknownCommand());
+
+
+
+       c = commands.getOrDefault(origin, new CommandUnknownCommand());
         
         return c;
     }
