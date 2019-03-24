08480162046de1baa0a61575086a811728d69bd8
Sebastian Bernth Jepsen <sbj@proxus.dk>
2019-03-24 17:02:34 +0100

    logicfacade methods

--- src/main/java/function/LogicFacade.java
+++ src/main/java/function/LogicFacade.java
@@ -1,7 +1,10 @@
 package function;
 
 import data.DataMapper;
+import data.Order;
 import data.User;
+import java.util.ArrayList;
+
 
 
 public class LogicFacade {
@@ -14,6 +17,22 @@ public class LogicFacade {
         User user = new User(email, password, username);
         DataMapper.createUser( user );
         return user;
+    }
+    
+    public boolean createOrder(String email, int height, int length, int width, int brick2x1, int brick2x2, int brick2x4, int orderId, boolean shipped){
+        try{
+            Order order = new Order(email, height, length, width, brick2x1, brick2x2, brick2x4, orderId, shipped);
+            DataMapper dm = new DataMapper();
+            dm.createOrder(order);
+            return true;
+        } catch (OrderException ex) {
+            return false;
+        }
+    }
+    
+    public ArrayList<Order> getOrders(String email) throws OrderException{
+        DataMapper dm = new DataMapper();
+        return dm.getOrders(email);
     }
 
 }
